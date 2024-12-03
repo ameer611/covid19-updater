23fb411ef073bfe2186370abd84013526c50c4f5
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-12-03 19:51:25 +0500

    update third party APIs

--- covid_updater.py
+++ covid_updater.py
@@ -5,8 +5,13 @@ import time
 
 
 def update():
-    r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
-    data = r.json()
+    # r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+    # data = r.json()
+    data = {
+        'cases': 10000000,
+        'deaths': 14235,
+        'recovered': 1000
+    }
     text = (f'Confirmed cases : {data["cases"]} '
             f'\nDeaths : {data["deaths"]} '
             f'\nRecovered : {data["recovered"]}')
